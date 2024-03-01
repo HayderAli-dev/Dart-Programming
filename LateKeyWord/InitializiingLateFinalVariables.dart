f6d6090c82bd9777d963e8add638b9eeb0cdb173
Haider Ali <100225999+HayderAli-dev@users.noreply.github.com>
2024-03-01 15:13:22 +0500

    Initial commit

--- LateKeyWord/InitializiingLateFinalVariables.dart
+++ LateKeyWord/InitializiingLateFinalVariables.dart
@@ -1,5 +1,15 @@
-void main(){
-
-
-  
+void main() {
+  late final bool isTeenager;
+  int age = 12;
+  if (age >= 13 && age <= 19) {
+    isTeenager = true;
+  } else if (age < 13) {
+    isTeenager = false;
+  }
+  //You simply cannot reinitilize final variables
+  try {
+    isTeenager = false;
+  } catch (e) {
+    print(e);
+  }
 }
